811105d947805ee07ed7e86807dede7ba9b83437
simeon amiase <amiase97@gmail.com>
2023-06-05 22:22:02 +0100

    added loader to the react page before loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
-
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
-import HeadCards from "./components/HeadCards"
-import Food from "./components/Food"
-import './Button.css'
-import Category from "./components/Category"
-import Footer from "./components/Footer"
+import { useState, useEffect } from "react";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import HeadCards from "./components/HeadCards";
+import Food from "./components/Food";
+import "./Button.css";
+import Category from "./components/Category";
+import Footer from "./components/Footer";
+import BounceLoader from "react-spinners/BounceLoader";
 
 function App() {
-  
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
 
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <HeadCards />
-      <Food />
-      <Category />
-      <Footer />
-    </div>
-
-  )
+    <>
+      {loading ? (
+        <div className="flex justify-center items-center h-screen">
+          <BounceLoader color={"#F97316"} loading={loading} size={150} speedMultiplier={2} />
+        </div>
+      ) : (
+        <div>
+          <Navbar />
+          <Hero />
+          <HeadCards />
+          <Food />
+          <Category />
+          <Footer />
+        </div>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
